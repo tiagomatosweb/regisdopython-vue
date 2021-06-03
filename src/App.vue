ff962568bca6eb12680cf896a80b5c29a4386e3f
Tiago Matos <tiago@tiagomatos.com>
2021-06-04 08:59:04 +1000

    Add fetch on users

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,36 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div
+        class="container pt-5"
+        id="app"
+    >
+        <div
+            v-for="user in users"
+            :key="user.id"
+            class="card mb-3"
+        >
+            <div class="card-body">
+                {{ user.name }}
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+
+    data() {
+        return {
+            users: [],
+        };
+    },
+
+    mounted() {
+        fetch('http://127.0.0.1:8000/api/users')
+            .then(response => response.json())
+            .then((res) => {
+                this.users = res.data;
+            });
+    },
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
