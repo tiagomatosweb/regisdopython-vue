fee041464637e75d0bb05dc642965b11c81d26a2
Tiago Matos <tiago@tiagomatos.com>
2021-06-04 09:00:51 +1000

    Add vue router plugin

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,32 @@
 <template>
-    <div
-        class="container pt-5"
-        id="app"
-    >
-        <div
-            v-for="user in users"
-            :key="user.id"
-            class="card mb-3"
-        >
-            <div class="card-body">
-                {{ user.name }}
-            </div>
-        </div>
+  <div id="app">
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
     </div>
+    <router-view/>
+  </div>
 </template>
 
-<script>
-export default {
-    name: 'App',
-
-    data() {
-        return {
-            users: [],
-        };
-    },
-
-    mounted() {
-        fetch('http://127.0.0.1:8000/api/users')
-            .then(response => response.json())
-            .then((res) => {
-                this.users = res.data;
-            });
-    },
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
 }
-</script>
+
+#nav {
+  padding: 30px;
+}
+
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
